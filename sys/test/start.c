60b750e88c3beb7c98a938d8d1f09a4586897647
Matthew Via <via@omega.(none)>
2012-01-09 01:06:20 -0500

    Initial addition of unit testing code.

--- sys/test/start.c
+++ sys/test/start.c
@@ -2,9 +2,11 @@
 #include "physmem.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <check.h>
 
 int
-main() {
+oldmain() {
   struct kernel test_kernel;
   test_kernel.phys = test_physmem_alloc(&test_kernel, 1024);
 
@@ -27,4 +29,32 @@ main() {
 
 
   return 0;
+}
+
+START_TEST (test_physmem_page_alloc) {
+  fail_if(0);
+} END_TEST
+
+START_TEST (test_physmem_page_free) {
+  fail_if(0);
+} END_TEST
+
+Suite *
+main_suite() {
+  Suite *s = suite_create("Subsystems");
+  TCase *tc_physmem = tcase_create("Physmem");
+  tcase_add_test(tc_physmem, test_physmem_page_alloc);
+  tcase_add_test(tc_physmem, test_physmem_page_free);
+  suite_add_tcase(s, tc_physmem);
+  return s;
+}
+
+int main(void) {
+  int num_failed;
+  Suite *s = main_suite();
+  SRunner *sr = srunner_create(s);
+  srunner_run_all(sr, CK_VERBOSE);
+  num_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return (num_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
