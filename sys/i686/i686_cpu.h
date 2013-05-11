bf42c3bc51e3b3d6c711e487fd46f667d95c36ca
Matthew Via <via@matthewvia.info>
2013-05-11 00:06:33 -0400

    Add tss structure

--- sys/i686/i686_cpu.h
+++ sys/i686/i686_cpu.h
@@ -57,12 +57,68 @@ typedef enum {
   E_VE = 20,
 } i686_exception;
 
+struct i686_tss {
+  uint16 link;
+  uint16 link_h;
+
+  uint32 esp0;
+  uint16 ss0;
+  uint16 ss0_h;
+
+  uint32 esp1;
+  uint16 ss1;
+  uint16 ss1_h;
+
+  uint32 esp2;
+  uint16 ss2;
+  uint16 ss2_h;
+
+  uint32 cr3;
+  uint32 eip;
+  uint32 eflags;
+
+  uint32 eax;
+  uint32 ecx;
+  uint32 edx;
+  uint32 ebx;
+
+  uint32 esp;
+  uint32 ebp;
+
+  uint32 esi;
+  uint32 edi;
+
+  uint16 es;
+  uint16 es_h;
+
+  uint16 cs;
+  uint16 cs_h;
+
+  uint16 ss;
+  uint16 ss_h;
+
+  uint16 ds;
+  uint16 ds_h;
+
+  uint16 fs;
+  uint16 fs_h;
+
+  uint16 gs;
+  uint16 gs_h;
+
+  uint16 ldt;
+  uint16 ldt_h;
+
+  uint16 trap;
+  uint16 iomap;
+} __attribute__((__packed__));
+
 struct i686_cpu {
   struct cpu c;
   struct feeder_physmem feeder;
   struct i686_gdt_entry gdt[5] __attribute__((aligned(4)));
   struct i686_idt_entry idt[256] __attribute__((aligned(4)));
-
+  struct i686_tss tss __attribute__((aligned(4)));
 
   /* CPUID, GDT */
 };
