51b23da374e9ff1dc97fe0bf904e0684e43cf33d
Matthew Via <via@matthewvia.info>
2013-07-18 20:17:11 -0400

    ..Also push io port access syntax corrections.

--- sys/i686/i686_io.h
+++ sys/i686/i686_io.h
@@ -3,13 +3,13 @@
 
 static inline void 
 i686_outb(unsigned short addr, unsigned char byte) {
-  asm volatile( "outb %0, %1" : : "a"(byte), "Nd"(addr));
+  __asm__ volatile( "outb %0, %1" : : "a"(byte), "Nd"(addr));
 }
   
 static inline char 
 i686_inb(unsigned short addr) {
   unsigned char byte;
-  asm volatile( "inb %1, %0" : "=a"(byte), "Nd"(addr));
+  __asm__ volatile( "inb %1, %0" : "=a"(byte) : "Nd"(addr));
   return byte;
 }
 
