b62062769cdcda4b71df1288ceffc180f9fe7fd2
via <via@racklap.my.domain>
2013-11-05 20:48:20 -0500

    Some work on cleaning up tests.

--- sys/test/test_start.c
+++ sys/test/test_start.c
@@ -13,7 +13,9 @@ static const int ctor_marker = 0x11;
 static const int dtor_marker = 0x22;
 
 struct kernel test_kernel;
-struct cpu test_cpu;
+struct cpu test_cpu = {
+    .allocator = &test_allocator,
+};
 
 struct cpu *cpu() {
   return &test_cpu;
@@ -23,72 +25,69 @@ struct kernel *kernel() {
   return &test_kernel;
 }
 
+
 START_TEST (check_physmem_alloc) {
 
-  struct kernel test_kernel;
   const unsigned int n_pages = 24;
 
-  test_kernel.phys = test_physmem_alloc(&test_kernel, n_pages);
+  kernel()->phys = test_physmem_alloc(kernel(), n_pages);
 
-  fail_unless(test_kernel.phys->parent == &test_kernel);
-  fail_unless(test_kernel.phys->total_pages == n_pages);
-  fail_unless(test_kernel.phys->free_pages == n_pages);
+  fail_unless(kernel()->phys->parent == kernel());
+  fail_unless(kernel()->phys->total_pages == n_pages);
+  fail_unless(kernel()->phys->free_pages == n_pages);
 
 } END_TEST
 
 START_TEST (check_physmem_page_alloc) { 
   
-  struct kernel test_kernel;
   const unsigned int n_pages = 24;
   unsigned int count;
   physmem_error_t err;
   physaddr_t page;
 
-  test_kernel.phys = test_physmem_alloc(&test_kernel, n_pages);
+  kernel()->phys = test_physmem_alloc(kernel(), n_pages);
 
   for (count = 0; count < n_pages; ++count) {
-    err = physmem_page_alloc(test_kernel.phys, 0, &page);
+    err = physmem_page_alloc(kernel()->phys, 0, &page);
     fail_unless(err == PHYSMEM_SUCCESS);
-    fail_unless(test_kernel.phys->total_pages == n_pages);
-    fail_unless(test_kernel.phys->free_pages == n_pages - (count + 1));
+    fail_unless(kernel()->phys->total_pages == n_pages);
+    fail_unless(kernel()->phys->free_pages == n_pages - (count + 1));
   }
 
-  err = physmem_page_alloc(test_kernel.phys, 0, &page);
+  err = physmem_page_alloc(kernel()->phys, 0, &page);
   fail_if(err != PHYSMEM_ERR_OOM);
 
 } END_TEST
 
 START_TEST (check_physmem_page_free) {
 
-  struct kernel test_kernel;
   const unsigned int n_pages = 24;
   unsigned int count;
   physmem_error_t err;
   physaddr_t page;
 
-  test_kernel.phys = test_physmem_alloc(&test_kernel, n_pages);
+  kernel()->phys = test_physmem_alloc(kernel(), n_pages);
 
   for (count = 0; count < n_pages; ++count) {
-    err = physmem_page_alloc(test_kernel.phys, 0, &page);
+    err = physmem_page_alloc(kernel()->phys, 0, &page);
     fail_unless(err == PHYSMEM_SUCCESS);
   }
   
   for (count = 0; count < n_pages; ++count) {
-    err = physmem_page_free(test_kernel.phys, 0);
+    err = physmem_page_free(kernel()->phys, 0);
     fail_unless(err == PHYSMEM_SUCCESS);
-    fail_unless(test_kernel.phys->free_pages == count + 1);
+    fail_unless(kernel()->phys->free_pages == count + 1);
   }
 
 } END_TEST
 
 START_TEST (check_physmem_stats) {
 
-  struct kernel test_kernel;
   const unsigned int n_pages = 24;
   struct physmem_stats stats;
 
-  test_kernel.phys = test_physmem_alloc(&test_kernel, n_pages);
-  stats = physmem_stats_get(test_kernel.phys);
+  kernel()->phys = test_physmem_alloc(kernel(), n_pages);
+  stats = physmem_stats_get(kernel()->phys);
   fail_unless(stats.kernel_pages == n_pages);
   fail_unless(stats.free_pages == n_pages);
 
@@ -96,19 +95,18 @@ START_TEST (check_physmem_stats) {
 
 START_TEST (check_physmem_feeder_create) {
 
-  struct kernel test_kernel;
   const unsigned int n_pages = 24;
   const unsigned int kept_pages = 5;
   const unsigned int min_source_pages = 7;
   struct feeder_physmem f;
 
-  test_kernel.phys = test_physmem_alloc(&test_kernel, n_pages);
-  feeder_physmem_create(&f, test_kernel.phys, kept_pages, min_source_pages);
+  kernel()->phys = test_physmem_alloc(kernel(), n_pages);
+  feeder_physmem_create(&f, kernel()->phys, kept_pages, min_source_pages);
 
-  fail_unless(f.source == test_kernel.phys);
+  fail_unless(f.source == kernel()->phys);
   fail_unless(f.pages_to_keep == kept_pages);
   fail_unless(f.min_free_source_pages = min_source_pages);
-  fail_unless(f.p.parent == &test_kernel);
+  fail_unless(f.p.parent == kernel());
   fail_unless(strcmp(f.p.name, "feeder") == 0);
   fail_unless(LIST_EMPTY(&f.p.freelist));
   fail_unless(f.p.total_pages == 0);
@@ -119,8 +117,6 @@ START_TEST (check_physmem_feeder_create) {
 
 
 START_TEST (check_physmem_feeder_feeds_correctly) {
-
-  struct kernel test_kernel;
   const unsigned int n_pages = 24;
   const unsigned int kept_pages = 5;
   const unsigned int min_source_pages = 7;
@@ -129,15 +125,15 @@ START_TEST (check_physmem_feeder_feeds_correctly) {
   physmem_error_t err;
   physaddr_t addr;
 
-  test_kernel.phys = test_physmem_alloc(&test_kernel, n_pages);
-  feeder_physmem_create(&f, test_kernel.phys, kept_pages, min_source_pages);
+  kernel()->phys = test_physmem_alloc(kernel(), n_pages);
+  feeder_physmem_create(&f, kernel()->phys, kept_pages, min_source_pages);
 
   err = physmem_page_alloc((struct physmem *)&f, 0, &addr);
   fail_unless(err == PHYSMEM_SUCCESS);
   fail_unless(f.p.free_pages >= kept_pages);
   /* Make sure that while the source has sufficient free pages, the feeder will
    * remain replenished */
-  while (test_kernel.phys->free_pages > min_source_pages ) {
+  while (kernel()->phys->free_pages > min_source_pages ) {
     err = physmem_page_alloc((struct physmem *)&f, 0, &addr);
     fail_unless(err == PHYSMEM_SUCCESS);
   }
@@ -167,7 +163,6 @@ START_TEST (check_physmem_feeder_feeds_correctly) {
 
 START_TEST (check_physmem_feeder_frees_correctly) {
   
-  struct kernel test_kernel;
   const unsigned int n_pages = 24;
   const unsigned int kept_pages = 5;
   const unsigned int min_source_pages = 5;
@@ -175,25 +170,25 @@ START_TEST (check_physmem_feeder_frees_correctly) {
   struct feeder_physmem f;
   physaddr_t addr;
 
-  test_kernel.phys = test_physmem_alloc(&test_kernel, n_pages);
-  feeder_physmem_create(&f, test_kernel.phys, kept_pages, min_source_pages);
+  kernel()->phys = test_physmem_alloc(kernel(), n_pages);
+  feeder_physmem_create(&f, kernel()->phys, kept_pages, min_source_pages);
 
   for (count = n_pages; count > 0; --count)
     physmem_page_alloc((struct physmem *)&f, 0, &addr);
 
   fail_unless(f.p.free_pages == 0);
-  fail_unless(test_kernel.phys->free_pages == 0);
+  fail_unless(kernel()->phys->free_pages == 0);
 
   for (count = 1; count <= min_source_pages; ++count) {
     physmem_page_free(&f.p, 0);
     fail_unless(f.p.free_pages == 0);
-    fail_unless(test_kernel.phys->free_pages == count);
+    fail_unless(kernel()->phys->free_pages == count);
   }
 
   for (count = 1; count <= kept_pages; ++count) {
     physmem_page_free(&f.p, 0);
     fail_unless(f.p.free_pages == count);
-    fail_unless(test_kernel.phys->free_pages == min_source_pages);
+    fail_unless(kernel()->phys->free_pages == min_source_pages);
   }
   /* Need to implement max/min for kept pages, and test it */
 
@@ -226,11 +221,7 @@ START_TEST (check_kmem_cache_init) {
   struct kmem_cache *k = test_kmem_cache_alloc();
 
 
-  struct cpu c = {
-    .allocator = &test_allocator,
-  };
-
-  err = common_kmem_cache_init(k, &c, "test-slab-32", slab_size,
+  err = common_kmem_cache_init(k, cpu(), "test-slab-32", slab_size,
       check_kmem_cache_ctor, check_kmem_cache_dtor);
 
   fail_unless(err == KMEM_SUCCESS);
@@ -239,14 +230,14 @@ START_TEST (check_kmem_cache_init) {
   fail_unless(strcmp(k->name, "test-slab-32") == 0);
   fail_unless(k->objsize == slab_size);
   fail_unless(k->used == 0);
-  fail_unless(k->cpu == &c);
+  fail_unless(k->cpu == cpu());
   fail_unless(SLIST_EMPTY(&k->slabs_empty));
   fail_unless(SLIST_EMPTY(&k->slabs_full));
   fail_unless(SLIST_EMPTY(&k->slabs_partial));
   fail_unless(k->v == &test_allocator.cv);
 
   /*Now test case of size < 0 */
-  err = common_kmem_cache_init(k, &c, "test-slab-0", 0,
+  err = common_kmem_cache_init(k, cpu(), "test-slab-0", 0,
       check_kmem_cache_ctor, check_kmem_cache_dtor);
   fail_unless(err == KMEM_ERR_INVALID);
 
